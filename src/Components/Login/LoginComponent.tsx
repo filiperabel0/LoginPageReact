3c9b346503daaaa93ebf1eba6fa9716ec065e572
Filipe Rabelo <filipindsgnr@gmail.com>
2022-09-09 09:41:54 -0300

    Backgorund Alterations and Animated

--- src/Components/Login/LoginComponent.tsx
+++ src/Components/Login/LoginComponent.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Login: React.FC = () => {
   return (
     <div className="{className}">
       <div className="card-login">
         <h1>Best Points</h1>
-        <input type="text" className='input-user' placeholder='User'/>
-        <input type="password" className='input-password' placeholder='Password'/>
-        <div className='div-btns'>
-        <button type='submit' className='btnSubmit'>Login</button>
-        <button type='submit' className='btnSubmit'>Cadastrar</button>
+        <input type="text" className="input-user" placeholder="User" />
+        <input
+          type="password"
+          className="input-password"
+          placeholder="Password"
+        />
+        <div className="div-btns">
+          <button type="submit" className="btnSubmit">
+            Login
+          </button>
+          <button type="submit" className="btnSubmit">
+            Cadastrar
+          </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
