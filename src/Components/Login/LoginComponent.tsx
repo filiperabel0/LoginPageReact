a73e310c1ab32a804d5bdddae8ae845f512e282d
Filipe Rabelo <filipindsgnr@gmail.com>
2022-09-09 08:54:34 -0300

    Alterações1.0

--- src/Components/Login/LoginComponent.tsx
+++ src/Components/Login/LoginComponent.tsx
@@ -6,9 +6,13 @@ const Login: React.FC = () => {
   return (
     <div className="{className}">
       <div className="card-login">
+        <h1>Best Points</h1>
         <input type="text" className='input-user' placeholder='User'/>
         <input type="password" className='input-password' placeholder='Password'/>
+        <div className='div-btns'>
         <button type='submit' className='btnSubmit'>Login</button>
+        <button type='submit' className='btnSubmit'>Cadastrar</button>
+        </div>
       </div>
     </div>
   )
--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import Login from '../Components/Login/LoginComponent'
+import '../Components/Login/index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>
 )
