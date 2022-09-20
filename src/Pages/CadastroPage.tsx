3262b52aa360739529daeb3a770c657bd31da6ea
MatheusSantos78 <teteusaba728@gmail.com>
2022-09-20 09:57:08 -0300

    feat: criação da pasta pages

---
+++ src/Pages/CadastroPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Cadastro from '../Components/Cadastro/CadastroComponent'
+import '../Components/Cadastro/index.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Cadastro />
+  </React.StrictMode>
+)
